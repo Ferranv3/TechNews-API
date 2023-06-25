f5b383ee589c3fe6ccc82d88c26b091cc469a90b
Ferranv3 <ferranhpv3@gmail.com>
2023-06-25 18:11:55 +0200

    Fix cors from frontend

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 // set up ======================================================================
 var express = require('express');
+var cors = require('cors');
 var app = express(); 						// create our app w/ express
 var port = process.env.PORT || 8080; 				// set the port
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 
-//app.use(express.static('./public')); 		// set the static files location /public/img will be /img for users
+var allowedOrigins = ['http://localhost:3000', 'https://technews-ferran.vercel.app/'];
+
+app.use(cors({
+  origin: function(origin, callback){
+    // permitir solicitudes sin 'origin' (como las de las aplicaciones móviles o curl)
+    if(!origin) return callback(null, true);
+   
+    if(allowedOrigins.indexOf(origin) === -1){
+      var msg = 'La política de CORS para este sitio no permite el acceso desde el origen especificado.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
 app.use(morgan('dev')); // log every request to the console
 app.use(bodyParser.urlencoded({'extended': 'true'})); // parse application/x-www-form-urlencoded
 app.use(bodyParser.json()); // parse application/json
