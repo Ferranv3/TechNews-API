03d2ac90005cec04667684bcdcd74575956061ae
Ferranv3 <ferranhpv3@gmail.com>
2023-06-26 17:00:17 +0200

    Implemented puppeter but commented for performance

--- app/scrapers/elChapuzas.js
+++ app/scrapers/elChapuzas.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
+const puppeteer = require('puppeteer');
 
 async function scrapeElChapuzas() {
     const url = 'https://elchapuzasinformatico.com/';
@@ -8,19 +9,34 @@ async function scrapeElChapuzas() {
     const $ = cheerio.load(html);
     const articles = $('article.blog-item');
 
-    const scrapedData = [];
-    articles.each((index, element) => {
-        const link = $(element).find('a.rocket-lazyload');
-        const title = $(element).find('h2.entry-title a').text();
-        const description = $(element).find('p.post-excerpt').text();
-        const href = link.attr('href');
-        //const img = $(element).find('a.rocket-lazyload img').attr('src');
-        //const style = 'background-image: url("https://elchapuzasinformatico.com/wp-content/uploads/2023/06/OceanGate-Titan-controlado-por-Logitech-F710-378x150.jpg")';
-        
-        scrapedData.push({ title, description, href });
-    });
+    const scrapedData = await Promise.all(
+        articles.map(async (index, element) => {
+            const link = $(element).find('a.rocket-lazyload');
+            const title = $(element).find('h2.entry-title a').text();
+            const description = $(element).find('p.post-excerpt').text();
+            const href = link.attr('href');
+            const img = undefined;//await scrapeIMGArticle(href);
+            
+            return { title, description, href, img };
+        }).get()
+    );
 
     return scrapedData;
+}
+
+async function scrapeIMGArticle(url) {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    await page.goto(url, { waitUntil: 'networkidle2' });
+
+    const img = await page.evaluate(() => {
+        const imgElement = document.querySelector('div.post-content p img');
+        return imgElement ? imgElement.src : null;
+    });
+
+    await browser.close();
+    return img;
 }
 
 module.exports = scrapeElChapuzas;
