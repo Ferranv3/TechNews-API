d26cbb43ea4f451b13f17048b8fd988f061ad6f4
Ferranv3 <ferranhpv3@gmail.com>
2023-06-26 11:57:24 +0200

    Implemented scrapers from HardZone and ProfesionalReview

--- app/scrapers/hardZone.js
+++ app/scrapers/hardZone.js
@@ -2,21 +2,18 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 
 async function scrapeHardZone() {
-    const url = 'https://elchapuzasinformatico.com/';
+    const url = 'https://hardzone.es/';
     const response = await axios(url);
     const html = response.data;
     const $ = cheerio.load(html);
-    const articles = $('article.blog-item');
+    const articles = $('article.news-item');
 
     const scrapedData = [];
     articles.each((index, element) => {
         const link = $(element).find('a.rocket-lazyload');
-        const title = $(element).find('h2.entry-title a').text();
-        const description = $(element).find('p.post-excerpt').text();
+        const title = $(element).find('h2.title a').text();
+        const description = $(element).find('div.excerpt p').text();
         const href = link.attr('href');
-        //const img = $(element).find('a.rocket-lazyload img').attr('src');
-        //const style = 'background-image: url("https://elchapuzasinformatico.com/wp-content/uploads/2023/06/OceanGate-Titan-controlado-por-Logitech-F710-378x150.jpg")';
-        
         scrapedData.push({ title, description, href });
     });
 
